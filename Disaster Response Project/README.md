a70e9f1e763f054acfe97dc6d47687d149fbc29f
Kehinde Adigun <kehindeadiguno@gmail.com>
2021-11-11 23:05:49 +0100

    Removed git tracking on model updates

--- Disaster Response Project/README.md
+++ Disaster Response Project/README.md
@@ -58,7 +58,7 @@ In a Python script, `train_classifier.py`, that runs a machine learning pipeline
  - Exports the final model as a pickle file
 
 3. **Flask Web Application**
-A flask web app visualizes message traing data and provides a simple interface where workers can worker can input a new message and get classification results in several categories. Images below:
+A flask web app visualizes message training data and provides a simple interface where workers can worker can input a new message and get classification results in several categories. Images below:
 
 ![Web App Image 1](images/webapp-image1-min.jpg)
 ![Web App Image 2](images/webapp-image2-min.jpg)
@@ -74,11 +74,17 @@ A flask web app visualizes message traing data and provides a simple interface w
 
     - To run ML pipeline that trains classifier and saves to disk
         `python models/train_classifier.py data/DisasterResponse.db models/classifier.pkl`
+        (Current model training will take a while. > An hour.)
 
 2. Run the following command in the app's directory to run your web app.
     `python run.py`
 
 3. Go to http://172.16.1.208:3001/ on your local machine to access web app.
+
+
+## Dataset:
+Our data consists of messages sent during disaster events. This data is categorized segemented into various multiclass, imbalanced categories. The data is split between the `categories.csv` and `disaster_messages.csv` files and cleaned data is stored in `DisasteraResponse.db`.
+Classes are imbalanced and as such we have categories like the 'related' category making up the largest share of the messages. While the classifier in this repo does not handle this problem, we can deal with the data imbalance with a number of methods like oversampling/undersampling from underrepresented classes.
 
 
 ## Acknowledgements
--- Disaster Response Project/models/train_classifier.py
+++ Disaster Response Project/models/train_classifier.py
@@ -22,13 +22,48 @@ import re
 from sqlalchemy import create_engine
 from os import path
 
-sys.path.append('../data/')
-
-from process_data import check_inputs
-
 nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('wordnet')
+
+
+def is_path(filepath, checktype='dir'):
+    """Checks if a path or directory exists. 
+    
+    Args:
+    filepath str: A string representing a path or directory.
+    checktype str:  A string. Accepts values 'dir' for directory and 'file' for file. Default: 'dir'
+    
+    Returns:
+    A boolean value: true if the path or directory exists and false otherwise.
+     
+    """
+    if checktype == 'dir':
+        if not path.isdir(filepath):
+            print('WARNING: Save Path does not exist.')
+            return False
+    if checktype == 'file':
+        if not path.isfile(filepath):
+            print('WARNING: File path does not exist.')
+            return False
+    return True
+
+
+def check_inputs(inputs, file_types):
+    """Checks if multiple inputs exist as files or directories. Uses the is_path function. 
+    
+    Args:
+    inputs list or array of strings: Contains all the directories to check.
+    file_types list or array of strings: Contains the expected file type for each input. Each list value should be a string of value 'file' or 'dir'
+    
+    Returns:
+    A boolean value: true only if the all path or directories exist and false otherwise.
+     
+    """
+    for i, filepath in enumerate(inputs):
+        if not is_path(filepath, file_types[i]):
+            return False
+    return True
 
 
 def load_data(database_filepath):
@@ -66,7 +101,7 @@ def tokenize(text):
     clean_text = re.sub(r'[^a-zA-Z0-9]',' ',text)
     tokens = word_tokenize(clean_text)
     lemmatizer = WordNetLemmatizer()
-    return [lemmatizer.lemmatize(word).lower().strip() for word in tokens]
+    return [lemmatizer.lemmatize(word).lower().strip() for word in tokens if word not in stopwords.words("english")]
 
 def build_model():
     """
@@ -76,7 +111,7 @@ def build_model():
     A grid search multiclassification model with a randomforest estimator as base
     """
     
-    vectorizer = CountVectorizer(tokenizer=tokenize, stop_words=stopwords.words("english"))
+    vectorizer = CountVectorizer(tokenizer=tokenize)
     transformer = TfidfTransformer()
     classifier = MultiOutputClassifier(RandomForestClassifier(verbose=1))
     
